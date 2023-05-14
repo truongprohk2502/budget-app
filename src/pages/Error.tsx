098d228dd6d681ba1b821af5d1162c8f20fc7419
Nguyen Dinh Truong <nguyendinhtruong98@gmail.com>
2023-05-14 11:38:26 +0700

    Add error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,6 +1,31 @@
 import { FC } from "react";
+import { Link, useNavigate, useRouteError } from "react-router-dom";
+import { ArrowUturnLeftIcon, HomeIcon } from "@heroicons/react/24/solid";
+
+interface IError {
+  message?: string;
+  statusText: string;
+}
 
 const Error: FC = () => {
-  return <div>Error</div>;
+  const error = useRouteError() as IError;
+  const navigate = useNavigate();
+
+  return (
+    <div className="error">
+      <h1>Uh oh! We’ve got a problem.</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className="flex-md">
+        <button className="btn btn--dark" onClick={() => navigate(-1)}>
+          <ArrowUturnLeftIcon width={20} />
+          <span>Go Back</span>
+        </button>
+        <Link to="/" className="btn btn--dark">
+          <HomeIcon width={20} />
+          <span>Go home</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 export default Error;
